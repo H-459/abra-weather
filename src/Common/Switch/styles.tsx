a4119bad4b9ac5eba88d3209e3c9a69fd6fbed42
Elad Zucker <elad-zucker@abra-it.com>
2022-07-17 10:45:19 +0300

    .

---
+++ src/Common/Switch/styles.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components/macro";
+
+export const SwitchWrapper = styled.div``
+
+export const SwitchCheckbox = styled.input``
+
+export const SwitchRightThumbWrapper = styled.div``
+export const SwitchLeftThumbWrapper = styled.div``
+
+export const SwitchToggle = styled.label`  
+`;
