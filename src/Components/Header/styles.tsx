1aca3f28fb187c130e30682662e3701caeaebac8
Elad Zucker <elad-zucker@abra-it.com>
2022-08-10 20:08:48 +0300

    .

---
+++ src/Components/Header/styles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.div`
+  width: 100%;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1);
+  background-color: #48bae4;
+  height: 94px;
+`;
+
+export const Logo = styled.img`
+    
+`;
+
